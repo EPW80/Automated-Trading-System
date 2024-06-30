b453e19965d439a2416ca950972545942b5f7a40
EPW <erikpw009@google.com>
2024-06-29 22:00:22 -0700

    updates to frontend/json

--- etf-frontend/src/pubsub/PubSub.js
+++ etf-frontend/src/pubsub/PubSub.js
@@ -3,8 +3,8 @@
  */
 export class PubSub {
   constructor() {
-      // An object to store subscribers, with event names as keys and arrays of callback functions as values.
-      this.subscribers = {};
+    // An object to store subscribers, with event names as keys and arrays of callback functions as values.
+    this.subscribers = {};
   }
   /**
    * Subscribe to an event with a callback function.
@@ -12,12 +12,12 @@ export class PubSub {
    * @param callback - The callback function to be called when the event is published.
    */
   subscribe(event, callback) {
-      // If there are no subscribers for the event, initialize the array.
-      if (!this.subscribers[event]) {
-          this.subscribers[event] = [];
-      }
-      // Add the callback function to the subscribers array for the event.
-      this.subscribers[event].push(callback);
+    // If there are no subscribers for the event, initialize the array.
+    if (!this.subscribers[event]) {
+      this.subscribers[event] = [];
+    }
+    // Add the callback function to the subscribers array for the event.
+    this.subscribers[event].push(callback);
   }
   /**
    * Unsubscribe from an event.
@@ -25,11 +25,12 @@ export class PubSub {
    * @param callback - The callback function to be removed from the subscribers array.
    */
   unsubscribe(event, callback) {
-      // If there are no subscribers for the event, return early.
-      if (!this.subscribers[event])
-          return;
-      // Filter out the callback function from the subscribers array for the event.
-      this.subscribers[event] = this.subscribers[event].filter((subscriber) => subscriber !== callback);
+    // If there are no subscribers for the event, return early.
+    if (!this.subscribers[event]) return;
+    // Filter out the callback function from the subscribers array for the event.
+    this.subscribers[event] = this.subscribers[event].filter(
+      (subscriber) => subscriber !== callback
+    );
   }
   /**
    * Publish an event, calling all subscribed callback functions with the provided data.
@@ -37,11 +38,10 @@ export class PubSub {
    * @param data - The data to be passed to the callback functions.
    */
   publish(event, data) {
-      // If there are no subscribers for the event, return early.
-      if (!this.subscribers[event])
-          return;
-      // Call each callback function with the provided data.
-      this.subscribers[event].forEach((callback) => callback(data));
+    // If there are no subscribers for the event, return early.
+    if (!this.subscribers[event]) return;
+    // Call each callback function with the provided data.
+    this.subscribers[event].forEach((callback) => callback(data));
   }
 }
 // Create a single instance of PubSub to be used throughout the application.
